8c2d02a72d5ba32bfbde4240b48f3641dbb37cdc
shenma <liubin.lbin@alibaba-inc.com>
2016-05-23 18:23:47 +0800

    render list view

---
+++ android/app/src/main/java/com/awesomeproject/ExampleActvityPackage.java
@@ -0,0 +1,33 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by umeng on 4/21/16.
+ */
+public class ExampleActvityPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        ArrayList<NativeModule> list = new ArrayList();
+        list.add(new ToastModuleAndroid(reactContext));
+        return list;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
--- index.android.js
+++ index.android.js
@@ -2,49 +2,131 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-
+'use strict';
 import React, {
-  AppRegistry,
   Component,
+} from 'react';
+
+import {
+  AppRegistry,
+  Image,
   StyleSheet,
+  ListView,
   Text,
-  View
+  View,
 } from 'react-native';
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+
+var MOCKED_MOVIES_DATA = [
+{title: "牛逼", year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}}
+];
+
+var movie = MOCKED_MOVIES_DATA[0];
+
+
+
 class AwesomeProject extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        movies: null,
+        dataSource: new ListView.DataSource({
+          rowHasChanged:(row1,row2)=> row1!==row2,
+        }),
+        loaded:false
+      };
+    }
+
   render() {
+    if (!this.state.dataSource) {
+      return this.renderLoadingView();
+    }
+
+    // var movie = this.state.movies[0];
+    // return this.renderMovie(movie);
+    return (<ListView
+      dataSource = {this.state.dataSource}
+      renderRow = {this.renderMovie}
+      style = {styles.ListView}
+      />);
+  };
+
+  componentDidMount() {
+  console.warn("this is a did mount");
+   this.fetchData();
+ }
+// getting data from internet
+ fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.warn(responseData.movies);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+         loaded: true,
+        });
+      })
+      .done();
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+        <Text>
+          Loading movies...
         </Text>
       </View>
     );
   }
+
+  renderMovie(movie) {
+   return (
+     <View style={styles.container}>
+       <Image
+         source={{uri: movie.posters.thumbnail}}
+         style={styles.thumbnail}
+       />
+       <View style={styles.rightContainer}>
+         <Text style={styles.title}>{movie.title}</Text>
+         <Text style={styles.year}>{movie.year}</Text>
+       </View>
+     </View>
+   );
+ }
+
 }
 
-const styles = StyleSheet.create({
+
+
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  rightContainer:{
+    flex:1
+  },
+  title:{
+    fontSize:20,
+    marginBottom:8,
+    textAlign:'center'
+  },
+  year:{
+    textAlign:'center'
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
